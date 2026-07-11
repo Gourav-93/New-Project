1b585a35bb4824391ae9cce6f7fbf8148ca2f623
Gourav khore <gouravkhore656@gmail.com>
2026-07-11 12:43:26 +0530

    Update

--- src/main/java/auth/Controller/UserController.java
+++ src/main/java/auth/Controller/UserController.java
@@ -1,6 +1,7 @@
 package auth.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +13,16 @@ import auth.Repository.UserRepository;
 @RestController
 @CrossOrigin("*")
 public class UserController {
-    
+
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/api/user/register")
     public String registerUser(@RequestBody UserModel user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return "User registered successfully!";
     }
@@ -25,7 +30,8 @@ public class UserController {
     @PostMapping("/api/user/login")
     public boolean loginUser(@RequestBody UserModel user) {
         UserModel existingUser = userRepository.findByEmail(user.getEmail());
-        if (existingUser != null && existingUser.getPassword().equals(user.getPassword())) {
+        if (existingUser != null &&
+                passwordEncoder.matches(user.getPassword(), existingUser.getPassword())) {
             return true;
         } else {
             return false;
